4eec8bd641414897344a9da715c4de6c6de6d933
Unknown <parker.s822@stu.nebo.edu>
2017-12-11 12:38:55 -0700

    Made a Smooth Cam, Need to Finish. Fix Battery pick-up

---
+++ THE REAL GHOST HOUSE/Assets/Scripts/Smooth_Camera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Smooth_Camera : MonoBehaviour {
+	[SerializeField]Transform target;
+	[SerializeField]Vector3 defaultDistance = new Vector3(0f, 2f, -10f);
+	[SerializeField]float distanceDamp = 10f;
+	[SerializeField]float rotationalDamp = 10f;
+
+
+	Transform myT;
+
+	void Awake()
+	{
+		myT = transform;
+	}
+
+
+	void LateUpdate()
+	{
+		Vector3 toPos = target.position + (target.rotation * defaultDistance);
+		Vector3 curPos = Vector3.Lerp(myT.position, toPos, distanceDamp * Time.deltaTime);
+		myT.position = curPos;
+
+		Quaternion toRot = Quaternion.LookRotation(target.rotation * defaultDistance);
+		Quaternion curRot = Quaternion.Slerp(myT.position, toPos, distanceDamp * Time.deltaTime);
+		myT.rotation = curRot;
+	}
+}
