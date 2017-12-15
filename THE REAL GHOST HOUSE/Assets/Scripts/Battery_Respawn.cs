7d190b05b131ceaa82159758414fdd27332e767c
Unknown <parker.s822@stu.nebo.edu>
2017-12-15 13:06:33 -0700

    Started Battery Spawn Script

---
+++ THE REAL GHOST HOUSE/Assets/Scripts/Battery_Respawn.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Battery_Respawn : MonoBehaviour {
+
+	public GameObject Battery;
+
+	public Transform spawnPoint;
+
+	public float spawnTime;
+
+	public bool batSpawned;
+
+
+	// Use this for initialization
+	void Start () {
+		batSpawned = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(batSpawned = false){
+			StartCoroutine(SpawnBat());
+		}
+		else{
+			prin
+		}
+	}
+
+	void onTriggerEnter(Collision other){
+		if(other.GameObject.tag == "Battery"){
+			batSpawned = true;
+		}
+		else{
+			batSpawned = false
+		}
+	}
+	
+	IEnumerator spawnBat(){
+		yield return new WaitForSeconds(spawnTime);
+		Rigidbody clone;
+		(Rigidbody)Instantiate(Battery, spawnPoint.position, spawnPoint.rotation);
+		
+	}
+
+}
+
